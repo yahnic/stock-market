920b17432fd2d3ae391125777ad5652b58c7d16d
Yahnic <nditahyahnic@gmail.com>
2020-08-22 21:00:31 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StockItemComponent } from './stock/stock-item/stock-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StockItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-item',
+  templateUrl: './stock-item.component.html',
+  styleUrls: ['./stock-item.component.scss']
+})
+export class StockItemComponent implements OnInit {
+  public name: string;
+  public code: string;
+  public price: number;
+  public previousPrice: number;
+  public positiveChange: boolean;
+
+  constructor() { }
+  
+  ngOnInit() {
+  this.name = 'Test Stock Company';
+  this.code = 'TSC';
+  this.price = 85;
+  this.previousPrice = 80;
+  this.positiveChange = this.price >= this.previousPrice;
+  }
+  }
